a4811f059648024e5e2a267f49beeab86f1719b2
harisaini778 <harikumarsaini778@gmail.com>
2023-12-24 20:52:38 +0530

    added logic to book and cancel the slots

--- Backend/routes/meeting.js
+++ Backend/routes/meeting.js
@@ -19,6 +19,9 @@ router.get('/meetings', async(req,res)=>{
 //Post Request for booking the slot
 
 router.get('/book/:meetingId', async(req,res)=>{
+
+    const {meetingId} = req.params;
+
     try {
         const meeting = await Meeting.findByPk(meetingId);
         
@@ -26,6 +29,14 @@ router.get('/book/:meetingId', async(req,res)=>{
             return res.status(404).json({error : "Meeting not found"})
         }
 
+        if(meeting.bookedSlots < meeting.slots) {
+            meeting.bookedSlots +=1;
+            await meeting.save();
+            return res.status(200).json({message : "Slot booked successfully"});
+        }
+        else {
+            return res.status(400).json({error : "No slots are available"});
+        }
 
 
     } catch(error) {
@@ -33,4 +44,33 @@ router.get('/book/:meetingId', async(req,res)=>{
     res.status(500).json({error : "Server error"});
     }
 });
+
+// Cancel a booking 
+
+router.post('/cancel/:meetingId',async(req,res)=>{
+    const {meetingId} = req.params;
+    try {
+        const meeting = await Meeting.findByPk(meetingId);
+
+       if(!meeting) {
+        return res.status(404).json({error:"Meeting not found"});
+       }
+
+       if(meeting.bookedSlots > 0 ) {
+        meeting.bookedSlots = -1;
+        await meeting.save();
+        return res.status(200).json({message:"Booking cancelled successfully!"})
+
+       } else {
+
+       return res.status(404).json({error:"No booked slots to cancel"});
+
+       }
+
+    } catch(error){
+        return res.status(500).json({message:"Server error"});
+    }
+});
+
+module.exports = router;
 
